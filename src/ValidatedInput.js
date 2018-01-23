01601baf4dc5cfa184f2a92a2ad32a80af3619be
Prakash Sanker <praks5432@gmail.com>
2018-01-23 16:45:05 +0530

    init practice

---
+++ src/ValidatedInput.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Input } from "xelpmoc-core";
+// HINT: USE `xelpmoc-core` here.
+
+export default class ValidatedInput extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div />;
+  }
+}
+
+ValidatedInput.propTypes = {};
+
+ValidatedInput.defaultProps = {};
