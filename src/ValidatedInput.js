6ddc9447a2068d9bce53f65fb521541839bbde72
Truptimayee031 <shruti@xelpmoc.in>
2018-01-27 17:34:10 +0530

    used conditional rendering

--- src/ValidatedInput.js
+++ src/ValidatedInput.js
@@ -1,42 +1,53 @@
 import React, { Component } from "react";
-import {Input} from "xelpmoc-core";
+import { Input } from "xelpmoc-core";
 import PropTypes from "prop-types";
 // HINT: USE `xelpmoc-core` here.
 
 export default class ValidatedInput extends React.Component {
   constructor(props) {
     super(props);
-    this.state={
-      hasError:false
-    }
+    this.state = {
+      hasError: false
+    };
   }
   renderError() {
-    this.setState({hasError:true})
+    this.setState({ hasError: true });
   }
 
   handleError(value) {
-    if(this.props.errorFunc(value)){
+    if (this.props.errorFunc(value)) {
       this.renderError();
-      }
-  }
-
-  render() {
-    if(this.state.hasError== true){
-      return <div>
-      <div className='error'><Input onChange={(value)=>this.handleError(value)} hasError={this.state.hasError}/></div>;
-    </div>}
-    else{
-      return<div>
-        <Input onChange={(value)=>this.handleError(value)} hasError={this.state.hasError}/>
-      </div>
     }
+  }
+  render() {
+    const isError = this.state.hasError;
+    if (isError) {
+      return (
+        <div>
+          <div className="error">
+            <Input
+              onChange={value => this.handleError(value)}
+              hasError={this.state.hasError}
+            />
+          </div>;
+        </div>
+      );
+    }
+    return (
+      <div>
+        <Input
+          onChange={value => this.handleError(value)}
+          hasError={this.state.hasError}
+        />
+      </div>
+    );
   }
 }
 
 ValidatedInput.propTypes = {
-  onChange:PropTypes.func
+  onChange: PropTypes.func
 };
 
 ValidatedInput.defaultProps = {
-  hasError:false
+  hasError: false
 };
