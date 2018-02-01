042b94c6ff03b39bc99a6e8d21172e1fe5228c21
meghnaXelpmoc <meghna.sharma@xelpmoc.in>
2018-02-01 09:56:48 +0530

    practice

--- src/ValidatedInput.js
+++ src/ValidatedInput.js
@@ -1,19 +1,49 @@
 import React, { Component } from "react";
+import {Input} from "xelpmoc-core";
+import PropTypes from "prop-types";
+
 // HINT: USE `xelpmoc-core` here.
 
-export default class ValidatedInput extends React.Component {
+export default class ValidatedInput extends Component {
   constructor(props) {
     super(props);
+    this.state={
+      hasError:false
+    }
   }
-  renderError() {}
+  renderError() {
+    if(this.state.hasError)
+      return<div className="error">{this.props.errorMsg}</div>
+  }
 
-  handleError(value) {}
+  handleError(value) {
+    if(this.props.errorFunc(value)){
+      this.setState({
+        hasError: true
+      })
+    }
+    else {
+      this.setState({
+        hasError: false
+      })
+    }
+  }
 
   render() {
-    return <div />;
+    return (
+      <div className="ValidateInput">
+        <Input onChange={(value)=>this.handleError(value)}/>
+        {this.renderError()}
+      </div>
+    );
   }
 }
 
-ValidatedInput.propTypes = {};
+ValidatedInput.propTypes = {
+  errorFunc: PropTypes.func,
+  errorMsg:PropTypes.string
+};
 
-ValidatedInput.defaultProps = {};
+ValidatedInput.defaultProps = {
+  errorMsg:"Invalid Input"
+};
