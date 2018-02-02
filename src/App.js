740533e7c3b42b0f8580000693f2d7afc6ffab4a
shubhambajpayee <shubham.bajpai@xelpmoc.in>
2018-02-02 18:05:53 +0530

    made all changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
 class App extends Component {
   render() {
     return (
@@ -13,6 +12,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
       </div>
     );
   }
--- src/ValidatedInput.js
+++ src/ValidatedInput.js
@@ -1,19 +1,51 @@
 import React, { Component } from "react";
-// HINT: USE `xelpmoc-core` here.
-
+import propsType from "prop-types";
+import Input from "xelpmoc-core";
 export default class ValidatedInput extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      hasError: props.hasError,
+      errorMsg: props.errorMsg ? props.errorMsg : ""
+    };
   }
-  renderError() {}
+  renderError() {
+    this.setState({
+      hasError: true
+    });
+  }
 
-  handleError(value) {}
+  handleError(event) {
+    if (this.props.hasError.errorfunc(event)) {
+      this.renderError();
+    } else {
+      this.setState({
+        hasError: false
+      });
+    }
+    console.log(hasError)
+  }
 
   render() {
-    return <div />;
+    return (
+      <div className="ValidatedInput">
+        <Input type="text" onChange={event => this.handleError(event)} />
+        {this.state.hasError ? (
+          <div className="ValidatedInput"> {this.props.errorMsg} </div>
+        ) : (
+          ""
+        )}
+      </div>
+    );
   }
 }
 
-ValidatedInput.propTypes = {};
+ValidatedInput.propTypes = {
+  nameOf: propsType.string,
+  onChange: propsType.func,
+  hasError: propsType.string
+};
 
-ValidatedInput.defaultProps = {};
+ValidatedInput.defaultProps = {
+  errorMsg: ""
+};
